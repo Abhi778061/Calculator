899cccbc04bd233d70f229925458184a1f431887
Abhi778061 <abhi778061@gmail.com>
2025-10-05 21:37:38 +0530

    Add files via upload

---
+++ Calculator App/script.js
@@ -0,0 +1,22 @@
+const display = document.getElementById('display');
+const buttons = document.querySelectorAll('button');
+
+function appendToDisplay(input) {
+    display.value += input;
+}
+
+function clearDisplay() {
+    display.value = "";
+}
+
+function calculate() {
+    try{
+        display.value = eval(display.value);
+    }
+    catch(error){
+        display.value = "Error";
+    }
+}
+function removeDisplay() {
+    display.value = display.value.slice(0, -1);
+}
